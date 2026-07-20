8db9d52e60106fe348879aedcf3626b4cb4d5929
amritk <amrit.kahlon00@gmail.com>
2026-07-20 14:38:17 -0700

    docs: hand-tune ListMoons comment

--- planet.go
+++ planet.go
@@ -194,7 +194,7 @@ func (r *PlanetService) UploadImage(ctx context.Context, planetID int64, body Pl
 	return res, err
 }
 
-// Fetch every known moon in the galaxy, with orbital data.
+// HUMAN EDIT: lists moons, hand-tuned docs by the SDK team.
 //
 // Parameters:
 //     ctx: Context for the request.
